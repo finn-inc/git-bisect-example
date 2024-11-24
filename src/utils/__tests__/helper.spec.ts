841dad9a13afba727eb5778b041abb2f3882a91b
Papillon6814 <so1rou.k@gmail.com>
2024-11-24 16:26:50 +0900

    refactor: test

--- src/utils/__tests__/helper.spec.ts
+++ src/utils/__tests__/helper.spec.ts
@@ -1,8 +1,13 @@
-import { calculateEditDistance } from '../helper';
+import { calculateEditDistance, helper } from '../helper';
 
-test('calculateEditDistance should return correct edit distance', () => {
-  expect(calculateEditDistance('kitten', 'sitting')).toBe(3);
-  expect(calculateEditDistance('flaw', 'lawn')).toBe(2);
-  expect(calculateEditDistance('intention', 'execution')).toBe(5);
+describe('helper', () => {
+  it('helper', () => {
+    expect(helper('hi')).toBe('helper sayss: hi');
+  })
+
+  it('calculateEditDistance should return correct edit distance', () => {
+    expect(calculateEditDistance('kitten', 'sitting')).toBe(3);
+    expect(calculateEditDistance('flaw', 'lawn')).toBe(2);
+    expect(calculateEditDistance('intention', 'execution')).toBe(5);
+  });
 });
-
