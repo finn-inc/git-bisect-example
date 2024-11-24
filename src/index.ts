29e76c153b8bb8abe129fed2a50a20fd9fd4d4c4
Papillon6814 <so1rou.k@gmail.com>
2024-11-24 16:36:28 +0900

    refactor: index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ import { helper, calculateEditDistance } from './utils/helper';
 
 console.log(helper('Hello World'));
 console.log(`Result of buggyFunction: ${calculateEditDistance('as', 'df')}`);
-
