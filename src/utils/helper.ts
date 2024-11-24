30b38fc898c105c4065d577be31f0efccbde256f
Papillon6814 <so1rou.k@gmail.com>
2024-11-24 16:32:36 +0900

    refactor: edit distance

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,19 +3,16 @@ export const helper = (msg: string): string => {
 }
 
 export const calculateEditDistance = (s1: string, s2: string): number => {
-  const len1 = s1.length;
-  const len2 = s2.length;
+  let previous = Array(s2.length + 1).fill(0);
+  let current = Array(s2.length + 1).fill(0);
 
-  let previous = Array(len2 + 1).fill(0);
-  let current = Array(len2 + 1).fill(0);
-
-  for (let j = 0; j <= len2; j++) {
+  for (let j = 0; j <= s2.length; j++) {
     previous[j] = j;
   }
 
-  for (let i = 1; i <= len1; i++) {
+  for (let i = 1; i <= s1.length; i++) {
     current[0] = i;
-    for (let j = 1; j <= len2; j++) {
+    for (let j = 1; j <= s2.length; j++) {
       if (s1[i - 1] === s2[j - 1]) {
         current[j] = previous[j - 1];
       } else {
@@ -29,5 +26,5 @@ export const calculateEditDistance = (s1: string, s2: string): number => {
     [previous, current] = [current, previous];
   }
 
-  return previous[len2];
+  return previous[s2.length];
 }
